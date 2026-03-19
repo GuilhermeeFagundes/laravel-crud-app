4f1485fbf3fa1e6edf9d243c859132ca7831a164
guilherme <guilhermefag2@outlook.com>
2026-03-19 20:22:37 -0300

    ajusta model usuarios conforme banco e adiciona relacionamento hasMany com produtos

--- app/Models/Usuarios.php
+++ app/Models/Usuarios.php
@@ -9,4 +9,24 @@ use Illuminate\Database\Eloquent\Model;
 class Usuarios extends Model
 {
     use HasFactory;
+
+    protected $table = 'usuarios';
+    protected $primarykey = 'usu_id';
+    
+    public $timestamps = false;
+
+    protected $fillable = [
+        'usu_nome',
+        'usu_email',
+        'usu_senha'
+    ];
+
+    //relacionamento
+
+    // 1:N - um para muitos (um usuario pode ter muitos produtos)
+
+    public function produtos(){
+            //$this->hasMany(ModeloRelacionado::class, 'FK','PK');
+        return $this->hasMany(Produtos::class, 'usu_id','usu_id'); //chave estrangeira - chave primária
+    }
 }
