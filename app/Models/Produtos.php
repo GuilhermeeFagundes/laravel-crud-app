fa55656ec74e83187d643a46b5c7d7c4d7ac773a
guilherme <guilhermefag2@outlook.com>
2026-03-25 19:32:48 -0300

    relacionamento entre produtos e usuários em produtos.php

--- app/Models/Produtos.php
+++ app/Models/Produtos.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Produtos extends Model
 {
     use HasFactory;
+
+    protected $table = 'produtos';
+    protected $primaryKey = "pro_id";
+
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'pro_nome',
+        'pro_descricao',
+        'pro_preco',
+        'usu_id'
+    ];
+
+
+    // Relacionamento com a tabela usuários
+    public function usuario() {
+        //belongsTo: um produto pertence a um usuário
+        // belongsTo:(Tabela relacionamnento, chave estrangeira, chave primária)
+        return $this->belongsTo(Usuarios::class, 'usu_id', 'usu_id');
+
+    }
+
+
 }
