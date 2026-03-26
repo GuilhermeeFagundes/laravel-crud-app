394bf2c5d217f2ae35d82006be2ba62862c71750
guilherme <guilhermefag2@outlook.com>
2026-03-26 19:56:26 -0300

    feat: select com parametros e insert de alunos

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -36,18 +36,68 @@ class AlunoController extends Controller
         $alunos = Aluno::where('id', '>', 5)->get()->toArray();
 
 
+        //Buscar todos os registros entre 5 e 10
+        $alunos = Aluno::whereBetween('id', [5, 10])->get()->toArray();
+
+        /* try {
+             //Buscar um registro pelo id
+            $aluno = Aluno::find(100);
+             echo "ID: ".$aluno['id']." - NOME: ".$aluno['nome']. " Curso: ".$aluno['curso']."<hr>";
+
+             } catch (\Exception $e) {
+             echo "Registro não encontrado";
+         } */
+        
+        //Buscar o primeiro registro que atenda uma condição
+      try {
+            $aluno = Aluno::where('id', '>', 5)->first()->toArray();
+            echo "ID: ".$aluno['id']." - NOME: ".$aluno['nome']. " Curso: ".$aluno['curso']."<hr>";
+
+        } catch (\Exception $e) {
+            echo "Registro não encontrado";
+        }
+    
+    // select count(*) from alunos
+        $totalAlunos = Aluno::count();
+        echo "Total de alunos: " . $totalAlunos;
+
+
+    //select avg(id) from alunos
+    $mediaId = Aluno::avg('id');
+    echo "<br>Média dos Ids: ". $mediaId;
+
+    //select sum(id) from alunos
+    $somaId = Aluno::sum('id');
+    echo "<br>Soma dos Ids: ". $somaId;
+
+    //select max(id) from alunos
+    $maxId = Aluno::max('id');
+    echo "<br>Maximo dos Ids: ". $maxId;
+
+    //select min(id) from alunos
+    $minId = Aluno::min('id');
+    echo "<br>Minimo dos Ids: ". $minId;
+
         // echo "<pre>";
         // print_r($alunos);
         // echo"</pre>";
 
-        foreach($alunos as $aluno) {
-            echo "ID: ".$aluno['id']." - NOME: ".$aluno['nome']. " Curso: ".$aluno['curso']."<hr>";
-        }
+        // foreach($alunos as $aluno) {
+        //     echo "ID: ".$aluno['id']." - NOME: ".$aluno['nome']. " Curso: ".$aluno['curso']."<hr>";
+        // }
 
     }
 
     public function insert (){
         echo "<h1> INSERT </h1>";
+
+        // Inserir novo registro
+
+        $aluno = new Aluno();
+        $aluno->nome = "Maria Silva";
+        $aluno->curso = "Engenharia";
+        $aluno->matricula = "123";
+        $aluno->save();
     }
 
     public function update (){
